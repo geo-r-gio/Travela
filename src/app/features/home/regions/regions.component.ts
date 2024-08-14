e8966385a00def5e762c6fd845b9778f052ef6ee
Georgio Nassar <georgionassar16@gmail.com>
2024-08-14 17:21:41 +0300

    feat: home page ui

--- src/app/features/home/regions/regions.component.ts
+++ src/app/features/home/regions/regions.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../../../core/authentication/services/auth.service'
 import { SnackbarService } from '../../../shared/services/snackbar/snackbar.service';
 import { tap } from 'rxjs';
 import { CountryService } from '../../services/country/country.service';
+import { faMagnifyingGlass, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { capitalCity, Country, countryName, subRegion } from '../../../shared/models/country.model';
 
 @Component({
   selector: 'app-regions',
@@ -12,8 +14,21 @@ import { CountryService } from '../../services/country/country.service';
 export class RegionsComponent {
 
   errorMessage: string | null = null;
-  countries: any[] = [];
+  countriesName: countryName[] = [];
+  countries: Country[] =[];
   profile: any[] = [];
+
+  activeDropdown: 'subregion' | 'city' | 'language' | 'currency' | null = null;
+
+  subregions: subRegion[] = [];
+  selectedSubregion: string | null = null;
+  isDropdownOpen = false;
+
+  cities: capitalCity[] = [];
+  selectedCity: string | null = null;
+  
+  faSearch = faMagnifyingGlass;
+  faLocation = faLocationDot;
 
   constructor(private authService : AuthService, private snackbarService : SnackbarService, private countryService : CountryService){
     this.getProfile();
@@ -38,9 +53,31 @@ export class RegionsComponent {
     });
   }
 
+  toggleDropdown(dropdown: 'subregion') {
+    this.activeDropdown = this.activeDropdown === dropdown ? null : dropdown;
+    console.log(this.activeDropdown);
+  }
+
+  isDropdown(dropdown: 'subregion'): boolean {
+    console.log(this.activeDropdown === dropdown);
+    return this.activeDropdown === dropdown;
+  }
+
+  selectSubregion(subregion: subRegion) {
+    this.selectedSubregion = subregion.subregion;
+    this.isDropdownOpen = false; // Close dropdown after selection
+  }
+
+  selectCity(city: capitalCity) {
+    this.selectedCity = city.capital[0];
+    this.isDropdownOpen = false;
+  }
+
   getCountries(){
     this.countryService.getAllCountries().subscribe((data: any) => {
-      this.countries = data;
+      this.countriesName = data;
+      this.subregions = data;
+      this.cities = data;
     });
   }
 
